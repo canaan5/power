81f8a357998f6777ae2158e129efc6a19bfa4d48
Kesty Canaan <kestylove5@gmail.com>
2014-12-18 08:34:57 +0000

    Power improvements

--- src/Canaan5/Power/Power.php
+++ src/Canaan5/Power/Power.php
@@ -17,9 +17,48 @@ class Power
 		return $this->app->auth->user();
 	}
 
-    public function oga4Top($userId)
-    {
-        $user = \Group::find($userId);
-        echo $user->groups()->pluck('name');
-    }
+	/**
+	 * Check if the current logged in or a giving user is super_admin
+	 */
+	public function oga4Top($userId = null)
+	{
+		if ( ! is_null($userId))
+		{
+			if ( $this->group($userId) == 'Oga4Top')
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+		if ( $this->group($this->user()->id) == 'Oga4Top')
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	public function group($userId)
+	{
+		$user = \User::find($userId);
+
+		if ( $user )
+		{
+			foreach ( $user->groups as $g)
+			{
+				return $g->name;
+			}
+
+		} else {
+
+			return 'User not found';
+		}
+
+		return false;
+
+	}
+
+
 }
